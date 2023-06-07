a391940627903af352fe8b2e23b47470e5ca0595
AdarshMasekar <adarshmasekar@gmail.com>
2023-06-07 10:04:10 +0530

    minor changes

--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -11,21 +11,19 @@ const BookingPage = () => {
       <div className='container side'>
         <div className="left">
         <div>
-            <img className='showImage' src={Show?.image?.medium || defaultImage} alt={Show.name}/>
+            <img className='bookImage' src={Show?.image?.medium || defaultImage} alt={Show.name}/>
         </div>
-        <h4 className="name">{Show.name}</h4>
-        <span>
-            <br />
-            <p>Type: {Show.type}</p>
-        </span>
+            
+        <h4 className="showName">{Show.name}</h4>
         </div>
 
         <div className="right">
             <h3>Genres: {Show?.genres?.[0] + ' ' + Show?.genres?.[1]}</h3>
             <h3>Language : {Show.language}</h3>
+            <h3>Type: {Show.type}</h3>
             <h3>Rating : {Show.rating.average}</h3>
             <h3>year : {Show.ended}</h3>
-            <h3>summary : {Show.summary}</h3>
+            <h3>summary : {Show.summary? Show.summary:"summary not available"}</h3>
         </div>
     </div>
   );
--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import '../styles/App.css'
 const ShowCard = ({ Show }) => {
   const defaultImage =
     'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png';
@@ -12,19 +12,11 @@ const ShowCard = ({ Show }) => {
   };
 
   return (
-    <div className="Show">
-      <div>
+    <div className="Show" onClick={handleBookClick}>
+      <div className='image-container'>
         <img className="showImage" src={Show?.image?.medium || defaultImage} alt={Show.name} />
       </div>
-      <div>
-        <span>
-          <br />
-          <p>Genres: {Show?.genres?.[0] + ' ' + Show?.genres?.[1]} </p>
-          <p>Type: {Show.type}</p>
-        </span>
         <h4 className="name">{Show.name}</h4>
-      </div>
-      <button className='book' onClick={handleBookClick}>Book Show</button>
     </div>
   );
 };
